a260a9218da0ee71ca057f5ed9e5d8a4d19d2906
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2019-04-24 13:59:30 +0200

    Initial commit of hyperopt optimise

---
+++ hyperopt_optimize.py
@@ -0,0 +1,221 @@
+
+"""Auto-optimizing a neural network with Hyperopt (TPE algorithm)."""
+
+
+from neural_net import build_and_train, build_model
+from utils import print_json, save_json_result, load_best_hyperspace
+
+from keras.utils import plot_model
+import keras.backend as K
+from hyperopt import hp, tpe, fmin, Trials
+
+import pickle
+import os
+import traceback
+
+
+space = {
+    # This loguniform scale will multiply the learning rate, so as to make
+    # it vary exponentially, in a multiplicative fashion rather than in
+    # a linear fashion, to handle his exponentialy varying nature:
+    'lr_rate_mult': hp.loguniform('lr_rate_mult', -0.5, 0.5),
+    # L2 weight decay:
+    'l2_weight_reg_mult': hp.loguniform('l2_weight_reg_mult', -1.3, 1.3),
+    # Batch size fed for each gradient update
+    'batch_size': hp.quniform('batch_size', 100, 450, 5),
+    # Choice of optimizer:
+    'optimizer': hp.choice('optimizer', ['Adam', 'Nadam', 'RMSprop']),
+    # Coarse labels importance for weights updates:
+    'coarse_labels_weight': hp.uniform('coarse_labels_weight', 0.1, 0.7),
+    # Uniform distribution in finding appropriate dropout values, conv layers
+    'conv_dropout_drop_proba': hp.uniform('conv_dropout_proba', 0.0, 0.35),
+    # Uniform distribution in finding appropriate dropout values, FC layers
+    'fc_dropout_drop_proba': hp.uniform('fc_dropout_proba', 0.0, 0.6),
+    # Use batch normalisation at more places?
+    'use_BN': hp.choice('use_BN', [False, True]),
+
+    # Use a first convolution which is special?
+    'first_conv': hp.choice(
+        'first_conv', [None, hp.choice('first_conv_size', [3, 4])]
+    ),
+    # Use residual connections? If so, how many more to stack?
+    'residual': hp.choice(
+        'residual', [None, hp.quniform(
+            'residual_units', 1 - 0.499, 4 + 0.499, 1)]
+    ),
+    # Let's multiply the "default" number of hidden units:
+    'conv_hiddn_units_mult': hp.loguniform('conv_hiddn_units_mult', -0.6, 0.6),
+    # Number of conv+pool layers stacked:
+    'nb_conv_pool_layers': hp.choice('nb_conv_pool_layers', [2, 3]),
+    # Starting conv+pool layer for residual connections:
+    'conv_pool_res_start_idx': hp.quniform('conv_pool_res_start_idx', 0, 2, 1),
+    # The type of pooling used at each subsampling step:
+    'pooling_type': hp.choice('pooling_type', [
+        'max',  # Max pooling
+        'avg',  # Average pooling
+        'all_conv',  # All-convolutionnal: https://arxiv.org/pdf/1412.6806.pdf
+        'inception'  # Inspired from: https://arxiv.org/pdf/1602.07261.pdf
+    ]),
+    # The kernel_size for convolutions:
+    'conv_kernel_size': hp.quniform('conv_kernel_size', 2, 4, 1),
+    # The kernel_size for residual convolutions:
+    'res_conv_kernel_size': hp.quniform('res_conv_kernel_size', 2, 4, 1),
+
+    # Amount of fully-connected units after convolution feature map
+    'fc_units_1_mult': hp.loguniform('fc_units_1_mult', -0.6, 0.6),
+    # Use one more FC layer at output
+    'one_more_fc': hp.choice(
+        'one_more_fc', [None, hp.loguniform('fc_units_2_mult', -0.6, 0.6)]
+    ),
+    # Activations that are used everywhere
+    'activation': hp.choice('activation', ['relu', 'elu'])
+}
+
+
+def plot(hyperspace, file_name_prefix):
+    """Plot a model from it's hyperspace."""
+    model = build_model(hyperspace)
+    plot_model(
+        model,
+        to_file='{}.png'.format(file_name_prefix),
+        show_shapes=True
+    )
+    print("Saved model visualization to {}.png.".format(file_name_prefix))
+    K.clear_session()
+    del model
+
+
+def plot_base_model():
+    """Plot a basic demo model."""
+    space_base_demo_to_plot = {
+        'lr_rate_mult': 1.0,
+        'l2_weight_reg_mult': 1.0,
+        'batch_size': 300,
+        'optimizer': 'Nadam',
+        'coarse_labels_weight': 0.2,
+        'conv_dropout_drop_proba': 0.175,
+        'fc_dropout_drop_proba': 0.3,
+        'use_BN': True,
+
+        'first_conv': 4,
+        'residual': 4,
+        'conv_hiddn_units_mult': 1.0,
+        'nb_conv_pool_layers': 3,
+        'conv_pool_res_start_idx': 0.0,
+        'pooling_type': 'inception',
+        'conv_kernel_size': 3.0,
+        'res_conv_kernel_size': 3.0,
+
+        'fc_units_1_mult': 1.0,
+        'one_more_fc': 1.0,
+        'activation': 'elu'
+    }
+    plot(space_base_demo_to_plot, "model_demo")
+
+
+def plot_best_model():
+    """Plot the best model found yet."""
+    space_best_model = load_best_hyperspace()
+    if space_best_model is None:
+        print("No best model to plot. Continuing...")
+        return
+
+    print("Best hyperspace yet:")
+    print_json(space_best_model)
+    plot(space_best_model, "model_best")
+
+
+def optimize_cnn(hype_space):
+    """Build a convolutional neural network and train it."""
+    try:
+        model, model_name, result, _ = build_and_train(hype_space)
+
+        # Save training results to disks with unique filenames
+        save_json_result(model_name, result)
+
+        K.clear_session()
+        del model
+
+        return result
+
+    except Exception as err:
+        try:
+            K.clear_session()
+        except:
+            pass
+        err_str = str(err)
+        print(err_str)
+        traceback_str = str(traceback.format_exc())
+        print(traceback_str)
+        return {
+            'status': STATUS_FAIL,
+            'err': err_str,
+            'traceback': traceback_str
+        }
+
+    print("\n\n")
+
+
+def run_a_trial():
+    """Run one TPE meta optimisation step and save its results."""
+    max_evals = nb_evals = 1
+
+    print("Attempt to resume a past training if it exists:")
+
+    try:
+        # https://github.com/hyperopt/hyperopt/issues/267
+        trials = pickle.load(open("results.pkl", "rb"))
+        print("Found saved Trials! Loading...")
+        max_evals = len(trials.trials) + nb_evals
+        print("Rerunning from {} trials to add another one.".format(
+            len(trials.trials)))
+    except:
+        trials = Trials()
+        print("Starting from scratch: new trials.")
+
+    best = fmin(
+        optimize_cnn,
+        space,
+        algo=tpe.suggest,
+        trials=trials,
+        max_evals=max_evals
+    )
+    pickle.dump(trials, open("results.pkl", "wb"))
+
+    print("\nOPTIMIZATION STEP COMPLETE.\n")
+
+
+if __name__ == "__main__":
+    """Plot the model and run the optimisation forever (and saves results)."""
+
+    print("Plotting a demo model that would represent "
+          "a quite normal model (or a bit more huge), "
+          "and then the best model...")
+
+    plot_base_model()
+
+    print("Now, we train many models, one after the other. "
+          "Note that hyperopt has support for cloud "
+          "distributed training using MongoDB.")
+
+    print("\nYour results will be saved in the folder named 'results/'. "
+          "You can sort that alphabetically and take the greatest one. "
+          "As you run the optimization, results are consinuously saved into a "
+          "'results.pkl' file, too. Re-running optimize.py will resume "
+          "the meta-optimization.\n")
+
+    while True:
+
+        # Optimize a new model with the TPE Algorithm:
+        print("OPTIMIZING NEW MODEL:")
+        try:
+            run_a_trial()
+        except Exception as err:
+            err_str = str(err)
+            print(err_str)
+            traceback_str = str(traceback.format_exc())
+            print(traceback_str)
+
+        # Replot best model since it may have changed:
+        print("PLOTTING BEST MODEL:")
+        plot_best_model()
---
+++ neural_net.py
@@ -0,0 +1,404 @@
+
+"""Convolutional neural network built with Keras."""
+
+from utils import print_json
+
+import keras
+from keras.datasets import cifar100  # from keras.datasets import cifar10
+from keras.layers.core import K  # import keras.backend as K
+from keras.optimizers import Adam, Nadam, RMSprop
+import tensorflow as tf
+from hyperopt import STATUS_OK, STATUS_FAIL
+
+import uuid
+import traceback
+import os
+
+
+TENSORBOARD_DIR = "TensorBoard/"
+WEIGHTS_DIR = "weights/"
+
+
+NB_CHANNELS = 3
+IMAGE_BORDER_LENGTH = 32
+# NB_CLASSES = 10
+NB_CLASSES_FINE = 100
+NB_CLASSES_COARSE = 20
+
+# (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+(_, y_train_c), (_, y_test_coarse) = cifar100.load_data(label_mode='coarse')
+(x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
+x_train = x_train.astype('float32') / 255.0 - 0.5
+x_test = x_test.astype('float32') / 255.0 - 0.5
+y_train = keras.utils.to_categorical(y_train, NB_CLASSES_FINE)
+y_test = keras.utils.to_categorical(y_test, NB_CLASSES_FINE)
+y_train_c = keras.utils.to_categorical(y_train_c, NB_CLASSES_COARSE)
+y_test_coarse = keras.utils.to_categorical(y_test_coarse, NB_CLASSES_COARSE)
+
+# You may want to reduce this considerably if you don't have a killer GPU:
+EPOCHS = 100
+STARTING_L2_REG = 0.0007
+
+OPTIMIZER_STR_TO_CLASS = {
+    'Adam': Adam,
+    'Nadam': Nadam,
+    'RMSprop': RMSprop
+}
+
+
+def build_and_train(hype_space, save_best_weights=False, log_for_tensorboard=False):
+    """Build the deep CNN model and train it."""
+    K.set_learning_phase(1)
+    K.set_image_data_format('channels_last')
+
+    # if log_for_tensorboard:
+    #     # We need a smaller batch size to not blow memory with tensorboard
+    #     hype_space["lr_rate_mult"] = hype_space["lr_rate_mult"] / 10.0
+    #     hype_space["batch_size"] = hype_space["batch_size"] / 10.0
+
+    model = build_model(hype_space)
+
+    # K.set_learning_phase(1)
+
+    model_uuid = str(uuid.uuid4())[:5]
+
+    callbacks = []
+
+    # Weight saving callback:
+    if save_best_weights:
+        weights_save_path = os.path.join(
+            WEIGHTS_DIR, '{}.hdf5'.format(model_uuid))
+        print("Model's weights will be saved to: {}".format(weights_save_path))
+        if not os.path.exists(WEIGHTS_DIR):
+            os.makedirs(WEIGHTS_DIR)
+
+        callbacks.append(keras.callbacks.ModelCheckpoint(
+            weights_save_path,
+            monitor='val_fine_outputs_acc',
+            save_best_only=True, mode='max'))
+
+    # TensorBoard logging callback:
+    log_path = None
+    if log_for_tensorboard:
+        log_path = os.path.join(TENSORBOARD_DIR, model_uuid)
+        print("Tensorboard log files will be saved to: {}".format(log_path))
+        if not os.path.exists(log_path):
+            os.makedirs(log_path)
+
+        # Right now Keras's TensorBoard callback and TensorBoard itself are not
+        # properly documented so we do not save embeddings (e.g.: for T-SNE).
+
+        # embeddings_metadata = {
+        #     # Dense layers only:
+        #     l.name: "../10000_test_classes_labels_on_1_row_in_plain_text.tsv"
+        #     for l in model.layers if 'dense' in l.name.lower()
+        # }
+
+        tb_callback = keras.callbacks.TensorBoard(
+            log_dir=log_path,
+            histogram_freq=2,
+            # write_images=True, # Enabling this line would require more than 5 GB at each `histogram_freq` epoch.
+            write_graph=True
+            # embeddings_freq=3,
+            # embeddings_layer_names=list(embeddings_metadata.keys()),
+            # embeddings_metadata=embeddings_metadata
+        )
+        tb_callback.set_model(model)
+        callbacks.append(tb_callback)
+
+    # Train net:
+    history = model.fit(
+        [x_train],
+        [y_train, y_train_c],
+        batch_size=int(hype_space['batch_size']),
+        epochs=EPOCHS,
+        shuffle=True,
+        verbose=1,
+        callbacks=callbacks,
+        validation_data=([x_test], [y_test, y_test_coarse])
+    ).history
+
+    # Test net:
+    K.set_learning_phase(0)
+    score = model.evaluate([x_test], [y_test, y_test_coarse], verbose=0)
+    max_acc = max(history['val_fine_outputs_acc'])
+
+    model_name = "model_{}_{}".format(str(max_acc), str(uuid.uuid4())[:5])
+    print("Model name: {}".format(model_name))
+
+    # Note: to restore the model, you'll need to have a keras callback to
+    # save the best weights and not the final weights. Only the result is
+    # saved.
+    print(history.keys())
+    print(history)
+    print(score)
+    result = {
+        # We plug "-val_fine_outputs_acc" as a
+        # minimizing metric named 'loss' by Hyperopt.
+        'loss': -max_acc,
+        'real_loss': score[0],
+        # Fine stats:
+        'fine_best_loss': min(history['val_fine_outputs_loss']),
+        'fine_best_accuracy': max(history['val_fine_outputs_acc']),
+        'fine_end_loss': score[1],
+        'fine_end_accuracy': score[3],
+        # Coarse stats:
+        'coarse_best_loss': min(history['val_coarse_outputs_loss']),
+        'coarse_best_accuracy': max(history['val_coarse_outputs_acc']),
+        'coarse_end_loss': score[2],
+        'coarse_end_accuracy': score[4],
+        # Misc:
+        'model_name': model_name,
+        'space': hype_space,
+        'history': history,
+        'status': STATUS_OK
+    }
+
+    print("RESULT:")
+    print_json(result)
+
+    return model, model_name, result, log_path
+
+
+def build_model(hype_space):
+    """Create model according to the hyperparameter space given."""
+    print("Hyperspace:")
+    print(hype_space)
+
+    input_layer = keras.layers.Input(
+        (IMAGE_BORDER_LENGTH, IMAGE_BORDER_LENGTH, NB_CHANNELS))
+
+    current_layer = random_image_mirror_left_right(input_layer)
+
+    if hype_space['first_conv'] is not None:
+        k = hype_space['first_conv']
+        current_layer = keras.layers.convolutional.Conv2D(
+            filters=16, kernel_size=(k, k), strides=(1, 1),
+            padding='same', activation=hype_space['activation'],
+            kernel_regularizer=keras.regularizers.l2(
+                STARTING_L2_REG * hype_space['l2_weight_reg_mult'])
+        )(current_layer)
+
+    # Core loop that stacks multiple conv+pool layers, with maybe some
+    # residual connections and other fluffs:
+    n_filters = int(40 * hype_space['conv_hiddn_units_mult'])
+    for i in range(hype_space['nb_conv_pool_layers']):
+        print(i)
+        print(n_filters)
+        print(current_layer._keras_shape)
+
+        current_layer = convolution(current_layer, n_filters, hype_space)
+        if hype_space['use_BN']:
+            current_layer = bn(current_layer)
+        print(current_layer._keras_shape)
+
+        deep_enough_for_res = hype_space['conv_pool_res_start_idx']
+        if i >= deep_enough_for_res and hype_space['residual'] is not None:
+            current_layer = residual(current_layer, n_filters, hype_space)
+            print(current_layer._keras_shape)
+
+        current_layer = auto_choose_pooling(
+            current_layer, n_filters, hype_space)
+        print(current_layer._keras_shape)
+
+        current_layer = dropout(current_layer, hype_space)
+
+        n_filters *= 2
+
+    # Fully Connected (FC) part:
+    current_layer = keras.layers.core.Flatten()(current_layer)
+    print(current_layer._keras_shape)
+
+    current_layer = keras.layers.core.Dense(
+        units=int(1000 * hype_space['fc_units_1_mult']),
+        activation=hype_space['activation'],
+        kernel_regularizer=keras.regularizers.l2(
+            STARTING_L2_REG * hype_space['l2_weight_reg_mult'])
+    )(current_layer)
+    print(current_layer._keras_shape)
+
+    current_layer = dropout(
+        current_layer, hype_space, for_convolution_else_fc=False)
+
+    if hype_space['one_more_fc'] is not None:
+        current_layer = keras.layers.core.Dense(
+            units=int(750 * hype_space['one_more_fc']),
+            activation=hype_space['activation'],
+            kernel_regularizer=keras.regularizers.l2(
+                STARTING_L2_REG * hype_space['l2_weight_reg_mult'])
+        )(current_layer)
+        print(current_layer._keras_shape)
+
+        current_layer = dropout(
+            current_layer, hype_space, for_convolution_else_fc=False)
+
+    # Two heads as outputs:
+    fine_outputs = keras.layers.core.Dense(
+        units=NB_CLASSES_FINE,
+        activation="sigmoid",
+        kernel_regularizer=keras.regularizers.l2(
+            STARTING_L2_REG * hype_space['l2_weight_reg_mult']),
+        name='fine_outputs'
+    )(current_layer)
+
+    coarse_outputs = keras.layers.core.Dense(
+        units=NB_CLASSES_COARSE,
+        activation="sigmoid",
+        kernel_regularizer=keras.regularizers.l2(
+            STARTING_L2_REG * hype_space['l2_weight_reg_mult']),
+        name='coarse_outputs'
+    )(current_layer)
+
+    # Finalize model:
+    model = keras.models.Model(
+        inputs=[input_layer],
+        outputs=[fine_outputs, coarse_outputs]
+    )
+    model.compile(
+        optimizer=OPTIMIZER_STR_TO_CLASS[hype_space['optimizer']](
+            lr=0.001 * hype_space['lr_rate_mult']
+        ),
+        loss='categorical_crossentropy',
+        loss_weights=[1.0, hype_space['coarse_labels_weight']],
+        metrics=['accuracy']
+    )
+    return model
+
+
+def random_image_mirror_left_right(input_layer):
+    """
+    Flip each image left-right like in a mirror, randomly, even at test-time.
+
+    This acts as a data augmentation technique. See:
+    https://stackoverflow.com/questions/39574999/tensorflow-tf-image-functions-on-an-image-batch
+    """
+    return keras.layers.core.Lambda(function=lambda batch_imgs: tf.map_fn(
+        lambda img: tf.image.random_flip_left_right(img), batch_imgs
+    )
+    )(input_layer)
+
+
+def bn(prev_layer):
+    """Perform batch normalisation."""
+    return keras.layers.normalization.BatchNormalization()(prev_layer)
+
+
+def dropout(prev_layer, hype_space, for_convolution_else_fc=True):
+    """Add dropout after a layer."""
+    if for_convolution_else_fc:
+        return keras.layers.core.Dropout(
+            rate=hype_space['conv_dropout_drop_proba']
+        )(prev_layer)
+    else:
+        return keras.layers.core.Dropout(
+            rate=hype_space['fc_dropout_drop_proba']
+        )(prev_layer)
+
+
+def convolution(prev_layer, n_filters, hype_space, force_ksize=None):
+    """Basic convolution layer, parametrized by the hype_space."""
+    if force_ksize is not None:
+        k = force_ksize
+    else:
+        k = int(round(hype_space['conv_kernel_size']))
+    return keras.layers.convolutional.Conv2D(
+        filters=n_filters, kernel_size=(k, k), strides=(1, 1),
+        padding='same', activation=hype_space['activation'],
+        kernel_regularizer=keras.regularizers.l2(
+            STARTING_L2_REG * hype_space['l2_weight_reg_mult'])
+    )(prev_layer)
+
+
+def residual(prev_layer, n_filters, hype_space):
+    """Some sort of residual layer, parametrized by the hype_space."""
+    current_layer = prev_layer
+    for i in range(int(round(hype_space['residual']))):
+        lin_current_layer = keras.layers.convolutional.Conv2D(
+            filters=n_filters, kernel_size=(1, 1), strides=(1, 1),
+            padding='same', activation='linear',
+            kernel_regularizer=keras.regularizers.l2(
+                STARTING_L2_REG * hype_space['l2_weight_reg_mult'])
+        )(current_layer)
+
+        layer_to_add = dropout(current_layer, hype_space)
+        layer_to_add = convolution(
+            layer_to_add, n_filters, hype_space,
+            force_ksize=int(round(hype_space['res_conv_kernel_size'])))
+
+        current_layer = keras.layers.add([
+            lin_current_layer,
+            layer_to_add
+        ])
+        if hype_space['use_BN']:
+            current_layer = bn(current_layer)
+    if not hype_space['use_BN']:
+        current_layer = bn(current_layer)
+
+    return bn(current_layer)
+
+
+def auto_choose_pooling(prev_layer, n_filters, hype_space):
+    """Deal with pooling in convolution steps."""
+    if hype_space['pooling_type'] == 'all_conv':
+        current_layer = convolution_pooling(
+            prev_layer, n_filters, hype_space)
+
+    elif hype_space['pooling_type'] == 'inception':
+        current_layer = inception_reduction(prev_layer, n_filters, hype_space)
+
+    elif hype_space['pooling_type'] == 'avg':
+        current_layer = keras.layers.pooling.AveragePooling2D(
+            pool_size=(2, 2)
+        )(prev_layer)
+
+    else:  # 'max'
+        current_layer = keras.layers.pooling.MaxPooling2D(
+            pool_size=(2, 2)
+        )(prev_layer)
+
+    return current_layer
+
+
+def convolution_pooling(prev_layer, n_filters, hype_space):
+    """
+    Pooling with a convolution of stride 2.
+
+    See: https://arxiv.org/pdf/1412.6806.pdf
+    """
+    current_layer = keras.layers.convolutional.Conv2D(
+        filters=n_filters, kernel_size=(3, 3), strides=(2, 2),
+        padding='same', activation='linear',
+        kernel_regularizer=keras.regularizers.l2(
+            STARTING_L2_REG * hype_space['l2_weight_reg_mult'])
+    )(prev_layer)
+
+    if hype_space['use_BN']:
+        current_layer = bn(current_layer)
+
+    return current_layer
+
+
+def inception_reduction(prev_layer, n_filters, hype_space):
+    """
+    Reduction block, vaguely inspired from inception.
+
+    See: https://arxiv.org/pdf/1602.07261.pdf
+    """
+    n_filters_a = int(n_filters * 0.33 + 1)
+    n_filters = int(n_filters * 0.4 + 1)
+
+    conv1 = convolution(prev_layer, n_filters_a, hype_space, force_ksize=3)
+    conv1 = convolution_pooling(prev_layer, n_filters, hype_space)
+
+    conv2 = convolution(prev_layer, n_filters_a, hype_space, 1)
+    conv2 = convolution(conv2, n_filters, hype_space, 3)
+    conv2 = convolution_pooling(conv2, n_filters, hype_space)
+
+    conv3 = convolution(prev_layer, n_filters, hype_space, force_ksize=1)
+    conv3 = keras.layers.pooling.MaxPooling2D(
+        pool_size=(3, 3), strides=(2, 2), padding='same'
+    )(conv3)
+
+    current_layer = keras.layers.concatenate([conv1, conv2, conv3], axis=-1)
+
+    return current_layer
