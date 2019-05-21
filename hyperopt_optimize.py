c2223a436bd8d8b9d635018a6df687a7b4278934
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2019-05-21 10:49:55 +0200

    added bash.sh and optimize_cnn

--- hyperopt_optimize.py
+++ hyperopt_optimize.py
@@ -3,15 +3,16 @@
 
 
 from neural_net import build_and_train, build_model
+from optimize_cnn import optimize_cnn
 from utils import print_json, save_json_result, load_best_hyperspace
 
 from keras.utils import plot_model
 import keras.backend as K
-from hyperopt import hp, tpe, fmin, Trials
+from hyperopt import hp, tpe
 
-import pickle
-import os
 import traceback
+
+from gradient_sdk.hyper_parameter import hyper_tune
 
 
 space = {
@@ -96,7 +97,6 @@ def plot_base_model():
         'conv_dropout_drop_proba': 0.175,
         'fc_dropout_drop_proba': 0.3,
         'use_BN': True,
-
         'first_conv': 4,
         'residual': 4,
         'conv_hiddn_units_mult': 1.0,
@@ -125,64 +125,19 @@ def plot_best_model():
     plot(space_best_model, "model_best")
 
 
-def optimize_cnn(hype_space):
-    """Build a convolutional neural network and train it."""
-    try:
-        model, model_name, result, _ = build_and_train(hype_space)
-
-        # Save training results to disks with unique filenames
-        save_json_result(model_name, result)
-
-        K.clear_session()
-        del model
-
-        return result
-
-    except Exception as err:
-        try:
-            K.clear_session()
-        except:
-            pass
-        err_str = str(err)
-        print(err_str)
-        traceback_str = str(traceback.format_exc())
-        print(traceback_str)
-        return {
-            'status': STATUS_FAIL,
-            'err': err_str,
-            'traceback': traceback_str
-        }
-
-    print("\n\n")
-
-
 def run_a_trial():
     """Run one TPE meta optimisation step and save its results."""
-    max_evals = nb_evals = 1
 
     print("Attempt to resume a past training if it exists:")
-
-    try:
-        # https://github.com/hyperopt/hyperopt/issues/267
-        trials = pickle.load(open("results.pkl", "rb"))
-        print("Found saved Trials! Loading...")
-        max_evals = len(trials.trials) + nb_evals
-        print("Rerunning from {} trials to add another one.".format(
-            len(trials.trials)))
-    except:
-        trials = Trials()
-        print("Starting from scratch: new trials.")
-
-    best = fmin(
+    print("Running HyperTune...")
+    best = hyper_tune(
         optimize_cnn,
         space,
         algo=tpe.suggest,
-        trials=trials,
-        max_evals=max_evals
+        max_evals=10
     )
-    pickle.dump(trials, open("results.pkl", "wb"))
 
-    print("\nOPTIMIZATION STEP COMPLETE.\n")
+    return best
 
 
 if __name__ == "__main__":
@@ -209,7 +164,9 @@ if __name__ == "__main__":
         # Optimize a new model with the TPE Algorithm:
         print("OPTIMIZING NEW MODEL:")
         try:
-            run_a_trial()
+            best = run_a_trial()
+            print(best)
+            print("\nOPTIMIZATION STEP COMPLETE.\n")
         except Exception as err:
             err_str = str(err)
             print(err_str)
