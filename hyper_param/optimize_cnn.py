52c87f49cc50eed0b93e3909239540efa067443e
Paweł Mudlaff <mudlaff.p@gmail.com>
2019-06-04 21:55:28 +0200

    Move export model logic to separate function. Change way how to generate path for export model

--- hyper_param/optimize_cnn.py
+++ hyper_param/optimize_cnn.py
@@ -1,15 +1,11 @@
-import os
 import traceback
-import time
 
-from tensorflow.python.saved_model import builder as saved_model_builder, tag_constants
-from tensorflow.python.saved_model.signature_def_utils_impl import predict_signature_def
-
-from neural_net import build_and_train
-from utils import save_json_result, is_gpu_available
 import tensorflow as tf
 import keras.backend as K
 from hyperopt import STATUS_FAIL
+
+from neural_net import build_and_train
+from utils import save_json_result, is_gpu_available, export_model
 
 
 def optimize_cnn(hype_space):
@@ -28,20 +24,7 @@ def optimize_cnn(hype_space):
         # Save training results to disks with unique filenames
         save_json_result(model_name, result)
 
-        try:
-            # Export Model
-            export_dir = os.path.abspath(os.environ.get('PS_MODEL_PATH', os.getcwd() + os.path.join('models', time.strftime("%Y%m%d-%H%M%S"), '1')))
-            K.set_learning_phase(0)
-
-            builder = saved_model_builder.SavedModelBuilder(export_dir)
-            with K.get_session() as sess:
-                builder.add_meta_graph_and_variables(sess=sess,
-                                                     tags=[tag_constants.SERVING],
-                                                     # signature_def_map={'predict': signature}
-                                                     )
-                builder.save()
-        except Exception as e:
-            print('Model export has failed with error: {}'.format(e))
+        export_model(model_name)
 
         K.clear_session()
         del model
